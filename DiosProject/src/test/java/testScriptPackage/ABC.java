a833dd5a320c1dced92bf44acb7cec1a9e32ddf1
ankit <ankit@Piyush>
2020-04-12 17:28:50 +0530

    We added "ABC-TEST-Script" test Script into the GIT-hub

---
+++ DiosProject/src/test/java/testScriptPackage/ABC.java
@@ -0,0 +1,15 @@
+package testScriptPackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ABC {
+
+	@Test
+	public void sss()
+	{
+		Reporter.log("ABC-TEST-Script",true);
+	}
+	
+	
+}
